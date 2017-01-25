38a73925a139c240f8f0286c6e04d1fcaed1f6f5
Technopriest90 <erokhin.ag@yandex.ru>
2017-01-25 16:07:24 +0300

    123

--- 1_6/Program.cs
+++ 1_6/Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace _1_6
-{
-    class Program
+    class Building // Программа, в которой используется класс Building.
     {
-        static void Main(string[] args)
-        {
-        }
+    public int Floors; // количество этажей
+    public int Area; // общая площадь здания
+    public int Occupants; // количество жильцов
+    }
+
+    // в этом классе объявляется объект типа Building.
+    class BuildingDemo
+{
+    static void Main()
+    {
+        Building house = new Building(); //создать объект типа Building
+        Building office = new Building();
+        int areaPP; // площадь на одного человека
+
+        //присвоить значения полям в объекте house.
+        house.Occupants = 4;
+        house.Area = 2500;
+        house.Floors = 2;
+
+        office.Occupants = 25;
+        office.Area = 4200;
+        office.Floors = 3;
+
+
+        //вычислить площадь на одного человек.
+        areaPP = house.Area / house.Occupants;
+
+        Console.WriteLine("Дом имеет:\n " + house.Floors + " этажа\n " + house.Occupants + " жильца\n " + house.Area + "кв. футов общей площади, из них\n " + areaPP + " приходится на одного человека");
+
+        //вычислить площадь на одного человек в учреждении.
+        areaPP = office.Area / office.Occupants;
+        Console.WriteLine("Офис имеет:\n " + office.Floors + " этажа\n " + office.Occupants + " рабочих\n " + office.Area + "кв. футов общей площади, из них\n " + areaPP + " приходится на одного человека");
+        Console.ReadKey();
     }
 }
